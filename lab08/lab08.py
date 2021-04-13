e16648a00596315c7364a95d225158382bbc4f3e
Michal <Michal@10.0.0.172>
2021-04-12 21:42:50 -0500

    Lab Code so far

--- lab08/lab08.py
+++ lab08/lab08.py
@@ -217,6 +217,7 @@ def test_median_3():
 def topk(items, k, keyf):
     ### BEGIN SOLUTION
     #for 
+    #retry
         #if items[i]>parent
             #replace parent
             #sort heap accordingly with lowest value as the parent value
